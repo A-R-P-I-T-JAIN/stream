9ebac0ebb84cad2eb86dd50e230c989747ca33ad
A-R-P-I-T-JAIN <116783129+A-R-P-I-T-JAIN@users.noreply.github.com>
2024-03-16 14:27:58 +0530

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,201 @@
-import Image from "next/image";
+"use client";
 
+import Image from "next/image";
+import meet from "../../public/meet.png";
+import meet2 from "../../public/meet2.png";
+import arrow from "../../public/arrow.png";
+import { useRouter } from "next/navigation";
+import { useEffect, useRef, useState } from "react";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import Card from "@/components/Card";
 export default function Home() {
+  const router = useRouter();
+  const [joinId, setJoinId] = useState("");
+
+  function randomID(len) {
+    let result = "";
+    if (result) return result;
+    var chars =
+        "12345qwertyuiopasdfgh67890jklmnbvcxzMNBVCZXASDQWERTYHGFUIOLKJP",
+      maxPos = chars.length,
+      i;
+    len = len || 5;
+    for (i = 0; i < len; i++) {
+      result += chars.charAt(Math.floor(Math.random() * maxPos));
+    }
+    return result;
+  }
+
+  useGSAP(() => {
+    gsap.to(".head", {
+      ease: "power1.inOut",
+      y: 0,
+      opacity: 1,
+      duration: 1,
+    });
+
+    gsap.fromTo(
+      ".para",
+      {
+        opacity: 0,
+        y: 20,
+      },
+      {
+        opacity: 1,
+        y: 0,
+        delay: 1,
+        stagger: 0.1,
+        duration: 1,
+      }
+    );
+
+    gsap.to(".op", {
+      opacity: 1,
+      delay: 2,
+    });
+    gsap.fromTo(
+      "#img",
+      {
+        y: 10,
+      },
+      {
+        y: -10,
+        yoyo: true,
+        repeat: -1,
+        ease: "power1.inOut",
+        duration: 1,
+      }
+    );
+  }, []);
+
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+    <div className="relative">
+      <h1 className="absolute top-3 left-3 text-3xl">Stream</h1>
+      
+      <div className="relative w-screen min-h-screen flex flex-col md:flex-row items-center justify-around py-28 md:py-0 gap-10 md:gap-0">
+      <Image src={arrow} width={80} height={80} id="img"  className='absolute bottom-10 left-1/2 -translate-x-1/2' />
+        <div className="md:max-w-[50%] max-w-[90%] flex flex-col gap-2 md:gap-6">
+          <h1 className="head opacity-0 translate-y-24 text-4xl md:text-7xl font-bold">
+            Stream
+          </h1>
+          <p className=" opacity-0 para text-2xl md:text-4xl">
+            One platform, endless conversations...
+          </p>
+          <div className="op opacity-0 flex flex-col md:flex-row md:items-center gap-2">
+            <button
+              className="rounded-md bg-blue-300 py-2 px-3 text-black font-semibold"
+              onClick={() => router.push(`/${randomID(5)}`)}
+            >
+              New Meeting
+            </button>
+            <p>or</p>
+            <div className='flex gap-2 items-center' >
+            <input
+              className="py-2 rounded-md px-2 text-black"
+              type="text"
+              placeholder="Enter a code to join"
+              value={joinId}
+              onChange={(e) => setJoinId(e.target.value)}
             />
-          </a>
+            <p
+              className="text-xl cursor-pointer"
+              style={{
+                color: joinId ? "skyblue" : "grey",
+              }}
+              onClick={() => router.push(`/${joinId}`)}
+            >
+              Join
+            </p>
+            </div>
+          </div>
+        </div>
+
+        <div>
+          <Image
+            id="img"
+            src={meet}
+            width={700}
+            height={700}
+            alt="Picture of the author"
+          />
         </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className="w-screen bg-slate-800 flex items-center justify-center py-28">
+        <div className="border-2 border-gray-200 flex flex-col md:flex-row items-center justify-around py-14 px-4 rounded-3xl max-w-[80%] gap-10 md:gap-0">
+          <div className="md:max-w-[50%] flex flex-col gap-4">
+            <h1 className="md:text-6xl text-3xl">Drive more engaging meetings.</h1>
+            <p className='text-sm md:text-xl'>
+              Ensure your meetings are designed to give everyone the ability to
+              engage no matter their location, language, or communication style.
+            </p>
+          </div>
+          <div>
+            <Image
+              src={meet2}
+              width={250}
+              height={250}
+              style={{
+                borderRadius: "100%",
+              }}
+              alt="meet"
+            />
+          </div>
+        </div>
       </div>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+      <div className='py-20 px-10 flex flex-col gap-16' >
+        <h1 className='md:text-8xl text-5xl font-bold' >Do Business <br /> Better.</h1>
+        <div className='flex md:flex-row flex-col items-center gap-12' >
+          <Card h1="Inclusive meetings for all." p="Real-time translation. Closed captions. Noise removal, voice optimization, and people-focused views. Give everyone a seat at the table, no matter how or where they work." />
+          <Card h1="Engagement is everything." p="From custom stage views and immersive share for presentations to interactive polling and Q&A features, meetings can be more enjoyable and productive." />
+          <Card h1="Flexibility comes first." p="Schedule and join meetings across any device with a single tap. Go from desktop to phone to car with our Move to Mobile QR code feature and Apple CarPlay integration. And use our library of embedded apps to simplify workflows." />
+        </div>
       </div>
-    </main>
+
+      <footer className="bg-white rounded-lg shadow dark:bg-gray-900 m-4">
+        <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
+          <div className="sm:flex sm:items-center sm:justify-between">
+            <a
+              href="#"
+              className="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse"
+            >
+              <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+                Stream
+              </span>
+            </a>
+            <ul className="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
+              <li>
+                <a href="#" className="hover:underline me-4 md:me-6">
+                  About
+                </a>
+              </li>
+              <li>
+                <a href="#" className="hover:underline me-4 md:me-6">
+                  Privacy Policy
+                </a>
+              </li>
+              <li>
+                <a href="#" className="hover:underline me-4 md:me-6">
+                  Licensing
+                </a>
+              </li>
+              <li>
+                <a href="#" className="hover:underline">
+                  Contact
+                </a>
+              </li>
+            </ul>
+          </div>
+          <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+          <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
+            © 2024{" "}
+            . All Rights Reserved.
+          </span>
+        </div>
+      </footer>
+    </div>
   );
 }
